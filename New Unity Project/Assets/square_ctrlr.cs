67f2b7f3ec8ac1f39c47a3de4e1d03396c53e122
Jix <jitx@users.noreply.github.com>
2019-09-23 20:30:09 -0700

    Initial Commit v0.1
    - Added Unity source files

---
+++ New Unity Project/Assets/square_ctrlr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square_ctrlr : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector3(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        a.SetFloat("Speed", Mathf.Abs(move));
+    }
+}
