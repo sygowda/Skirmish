7e4dfa20c420d1a7433d5c4b0839452a9f2de533
sharonheai <50536069+sharonheai@users.noreply.github.com>
2019-09-28 16:00:49 -0700

    Add files via upload

---
+++ Skirmish/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+public class shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        //fire1 button needs to be determined
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+    }
+}
