594423cdeddb80ce2d8719d45e19dfb9915cc413
Shubham <shubham.gowda@gmail.com>
2019-09-28 16:31:56 -0700

    Movement

--- Skirmish/Assets/square_ctrlr.cs
+++ Skirmish/Assets/square_ctrlr.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class square_ctrlr : MonoBehaviour
 {
     public float maxSpeed = 10f;
-    Animator a;
+
     // Start is called before the first frame update
     void Start()
     {
-        a = GetComponent<Animator>();
+
         
     }
 
@@ -18,6 +18,6 @@ public class square_ctrlr : MonoBehaviour
     {
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector3(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        a.SetFloat("Speed", Mathf.Abs(move));
+
     }
 }
