5edcddc443203d8ac9b3a4f1ac69ad83e2334a4c
Shubham <shubham.gowda@gmail.com>
2019-09-28 16:32:42 -0700

    Movement

---
+++ Skirmish/Assets/gamemanger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1.0f;
+    public Vector2 moveVector;
+
+}
