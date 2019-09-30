ba20eaa65d16fb23d5ba04e0b27f6a69101bd658
Mita <mitashimpi@gmail.com>
2019-09-29 18:03:58 -0700

    Add script for destroying bricks

---
+++ Skirmish/Assets/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int weight = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random rand = new System.Random();
+        weight = rand.Next(10, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage( int damage)
+    {
+        weight -= damage;
+        if(weight <= 0)
+        {
+            Break();
+        }
+    }
+    private void Break()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Skirmish/Assets/Scripts/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        // Shoot();
+        //}
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime = nextActionTime + period;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
