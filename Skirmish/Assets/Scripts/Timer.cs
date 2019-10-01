07dd2f0bef9bd1ceb6908a9f852a825e8abb4e95
Mita <mitashimpi@gmail.com>
2019-09-30 22:13:58 -0700

    Add timer component and change speed of bullets

---
+++ Skirmish/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int remainingTime = 30; //Total seconds
+    public Text gameTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Make sure time passes at the same rate as real time
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer.text = ("" + remainingTime); //Show the remaining time on the screen
+
+        if(remainingTime <= 0)
+        {
+            StopCoroutine("LoseTime");
+            Application.Quit();
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            remainingTime--;
+        }
+    }
+}
