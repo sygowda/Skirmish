ab31b1a88769b985423e41891e99b0fd2947631b
zongbaol <zongbaol@usc.edu>
2019-10-07 15:23:11 -0400

    add health display to chests

--- Skirmish/Assets/Scripts/Chest.cs
+++ Skirmish/Assets/Scripts/Chest.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Chest : MonoBehaviour
 {
     // Start is called before the first frame update
     public int health;
+    public GameObject text;
+    TextMeshPro tmp;
     void Start()
     {
         health = 5;
+        tmp = text.GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tmp.text = health.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,7 +26,7 @@ public class Chest : MonoBehaviour
         if(collision.tag == "BulletTag")
         {
             Debug.Log(health);
-            if (health > 0)
+            if (health > 1)
             {
                 health--;
                 Debug.Log(health);
