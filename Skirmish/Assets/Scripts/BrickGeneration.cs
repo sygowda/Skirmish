a62d98d9b127346367c5198a40d75df7539429f0
Mita <mitashimpi@gmail.com>
2019-10-07 10:31:56 -0700

    Generate brick from prefab and change color bsaed on weight

---
+++ Skirmish/Assets/Scripts/BrickGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickGeneration : MonoBehaviour
+{
+    public int numberOfBricks = 6;
+    private float startXPosition = -2.5f;
+    public GameObject brickPrefab;
+    void Start()
+    {
+        for(int i = 0; i < numberOfBricks; i++)
+        {
+            Vector2 brickPosition = new Vector2(startXPosition + i, 0f);
+            Instantiate(brickPrefab, brickPosition, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
