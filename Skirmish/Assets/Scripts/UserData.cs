bfcb26c27c8bc2bc04660908786060fe9b8e1074
zongbaol <zongbaol@usc.edu>
2019-10-16 15:40:49 -0700

    Added UserData global data storage

---
+++ Skirmish/Assets/Scripts/UserData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UserData : MonoBehaviour
+{
+	private static string name;
+	private static int level, coin;
+
+	public static string name
+	{
+		get
+		{
+			return name;
+		}
+		set
+		{
+			name = value;
+		}
+	}
+	public static int level
+	{
+		get
+		{
+			return level;
+		}
+		set
+		{
+			level = value;
+		}
+	}
+	public static int coin
+	{
+		get
+		{
+			return coin;
+		}
+		set
+		{
+			coin = value;
+		}
+	}
+
+}
