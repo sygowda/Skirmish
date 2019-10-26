0424e143c221a794f3887edcf4d09bb528d3f756
JBeavans <beavans@usc.edu>
2019-10-26 16:25:07 -0700

    touch controls

---
+++ Skirmish/Assets/Scripts/p1Touch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p1Touch : MonoBehaviour
+{
+    public Transform p1cursorObject;
+    public float p1distance = 1.5f;
+    public bool p1drag = false;
+    public Touch p1_touch;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches) {
+            if (touch.phase == TouchPhase.Began && Camera.main.ScreenToWorldPoint(touch.position).y > 0f)
+            {
+                p1_touch = touch;
+            }
+        }
+        Ray ray = Camera.main.ScreenPointToRay(p1_touch.position);
+        Vector3 point = transform.position;
+        point.x = ray.origin.x + (ray.direction.x * p1distance);
+        //if (Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y > 0f)
+        //{
+            if (point.x > 2.553f) { point.x = 2.553f; }
+            if (point.x < -2.553f) { point.x = -2.553f; }
+
+            if ((p1cursorObject != null) && p1drag)
+                p1cursorObject.position = point;
+
+            if (p1_touch.phase == TouchPhase.Ended || p1_touch.phase == TouchPhase.Canceled)
+            {
+                p1drag = false;
+            } else
+            {
+                p1drag = true;
+            }                
+        //}
+    }
+}
+
+
---
+++ Skirmish/Assets/Scripts/p2Touch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2Touch : MonoBehaviour
+{
+    public Transform cursorObject;
+    public float distance = 1.5f;
+    public bool drag = false;
+    public static Touch p2_touch;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began && Camera.main.ScreenToWorldPoint(touch.position).y < 0f)
+            {
+                p2_touch = touch;
+            }
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(p2_touch.position);
+        Vector3 point = transform.position;
+        point.x = ray.origin.x + (ray.direction.x * distance);
+        //if (Camera.main.ScreenToWorldPoint(Input.mousePosition).y < 0f)
+        //{
+            if (point.x > 2.553f) { point.x = 2.553f; }
+            if (point.x < -2.553f) { point.x = -2.553f; }
+
+            if (drag)
+                cursorObject.position = point;
+
+            if (p2_touch.phase == TouchPhase.Ended || p2_touch.phase == TouchPhase.Canceled)
+            {
+                drag = false;
+            }
+            else
+            {
+                drag = true;
+            }
+
+        //}
+    }
+}
