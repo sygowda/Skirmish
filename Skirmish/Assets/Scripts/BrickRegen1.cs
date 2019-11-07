1a8c375c83e144febe769e6f505a0314e5b95ce7
Mita <mitashimpi@gmail.com>
2019-11-07 12:42:56 -0800

    Change bullet speed

--- Skirmish/Assets/Scripts/BrickRegen1.cs
+++ Skirmish/Assets/Scripts/BrickRegen1.cs
@@ -60,7 +60,7 @@ public class BrickRegen1 : MonoBehaviour
         {
             if(bricks[i].activeSelf == false)
             {
-                if (respawnTimeList[i].rate < 5) respawnTimeList[i].rate = Random.Range(5, 50);
+                if (respawnTimeList[i].rate < 10) respawnTimeList[i].rate = Random.Range(10, 70);
                 else
                 {
                     if (respawnTimeList[i].timeSinceLastSpawned >= respawnTimeList[i].rate)
